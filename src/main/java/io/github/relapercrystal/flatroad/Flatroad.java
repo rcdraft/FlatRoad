44b4a01599eb2d4c59b43d4b05f82b1d8dc1547a
RelaperCrystal <EndermanLOTR@outlook.com>
2020-10-21 10:42:49 +0800

    Initial commit

---
+++ src/main/java/io/github/relapercrystal/flatroad/Flatroad.java
@@ -0,0 +1,17 @@
+package io.github.relapercrystal.flatroad;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Flatroad extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
